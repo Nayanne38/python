e8c7a8364bcef2ae8c4419ae310187c9c3bfc293
Fabricya Nayanne Santos <126834600+Nayanne38@users.noreply.github.com>
2026-03-25 18:18:01 -0300

    Add files via upload

---
+++ python.leb/lista_inteiros.py
@@ -0,0 +1,9 @@
+numeros = [2, 4, 6, 8, 10]
+
+quantidade_itens = len(numeros)
+maximo_itens = max(numeros)
+minimo_itens = min(numeros)
+
+print(f"lista: {numeros}")
+print(f"Maximo de itens: {maximo_itens}")
+print(f"Minimo de Itens {minimo_itens}")
